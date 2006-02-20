d4971caa753551f8f32627ab21f99b89bde60a49
psxc <psxc@pzs-ng.com>
2006-02-20 22:36:55 +0000

    updated psxc-free with quota support and botlogging

--- psxc-free/psxc-free.sh
+++ psxc-free/psxc-free.sh
@@ -4,13 +4,15 @@
 conf=./psxc-free.conf
 
 ## code part below
+####################################################################
 
 logsimpledir()
 {
   if [[ -d $1 ]]; then
     mdate=$($MDATE $1 $2)
     dsize=$(du -sk $1 | awk '{print $1}')
-    echo -e "$mdate\t$dsize\t$1" >>$TEMPFILE1
+    sectionsize[$secnum]=$(expr ${sectionsize[$secnum]} + $dsize)
+    echo -e "$mdate\t$dsize\t$1\t$secnum" >>$TEMPFILE1
   fi
 }
 
@@ -32,8 +34,8 @@ logsubsubdir()
 
 logdir()
 {
-   tempvar=$1
-   tempvar=${tempvar%/\*}
+  tempvar=$1
+  tempvar=${tempvar%/\*}
   if [[ "$tempvar" != "$1" ]]; then
     logsubsubdir $tempvar
   else
@@ -47,8 +49,8 @@ freeupspace()
     if [[ "$TESTRUN" == "YES" ]]; then
       echo "REMOVING $1"
     else
-      echo "removing $1"
-      rm -fR $1
+      #rm -fR $1
+      echo "$(date "+%a %b %e %T %Y") PSXCFREE: {$(echo /$1 | sed "s|$GLROOT||" | tr -s '/')} {$2}" #>> $GLLOG
     fi
   else
     if [[ "$TESTRUN" == "YES" ]]; then
@@ -60,13 +62,12 @@ freeupspace()
 source $conf
 devnum=1
 eval devicename=\$DEVICENAME_$devnum
-:>$TEMPFILE1
-:>$TEMPFILE2
 
 while [ "$devicename" ]; do
+  :>$TEMPFILE1
+  :>$TEMPFILE2
   # filling vars
   eval minfree=\$MINFREE_$devnum
-echo $minfree
   eval setfree=\$SETFREE_$devnum
   eval dirs=\$DIRS_$devnum
   eval excludes=\$EXCLUDES_$devnum
@@ -74,7 +75,7 @@ echo $minfree
   eval daysback=\$DAYSBACK_$devnum
 
   # testing to see if enough space is availible on device
-  freespace=$(df -m $devicename | grep ^/ | awk '{print $4}')
+  freespace=$(df -Pm $devicename | grep ^/ | awk '{print $4}')
   if [[ -z "$freespace" ]]; then
     echo "FAILED! COULD NOT STAT $devicename!"
     let devnum=devnum+1
@@ -87,9 +88,16 @@ echo $minfree
     eval devicename=\$DEVICENAME_$devnum
     continue
   fi
-
   # availible space is below minimum allowed
-  for dname in $dirs; do
+  secnum=0
+  for modname in $dirs; do
+    let secnum=secnum+1
+    sectionsize[$secnum]=0
+    if [[ $(echo $modname | grep ":") ]]; then
+      dname=$(echo $modname | cut -d ":" -f 1)
+    else
+      dname=$modname
+    fi
     # dated dir structure ?
     dirname=$SITEDIR/$dname
     if [[ $(echo "$dirname" | grep "%") ]]; then
@@ -114,22 +122,62 @@ echo $minfree
       logdir "$dirname"
     fi
   done
-
   # now we have a list of dirs with dates and sizes. let's find out what we need to remove.
   sort $TEMPFILE1 >$TEMPFILE2
-  TMPIFS="$IFS"
-  IFS="
-"
-  for readline in $(cat $TEMPFILE2); do
-    freeupspace $(echo $readline | awk '{print $3}')
-    if [[ "$TESTRUN" == "YES" || ! -e $(echo $readline | awk '{print $3}') ]]; then
-      let freespace=freespace+$(echo $readline | awk '{print $2}')/1024
-      if [[ $freespace -gt $setfree ]]; then
-        break
+  secnum=0
+  for modname in $dirs; do
+    let secnum=secnum+1
+    if [[ ! -z "$(echo $modname | grep ":")" ]]; then
+      dname=$(echo $modname | cut -d ":" -f 1)
+      ssize=$(echo $modname | cut -d ":" -f 2)
+      if [[ $(echo $ssize | grep -i "G") ]]; then
+        dsize=$(echo $ssize | tr -cd '0-9')
+        let dsize=dsize*1024
+      elif [[ $(echo $ssize | grep -i "T") ]]; then
+        dsize=$(echo $ssize | tr -cd '0-9')
+        let dsize=dsize*1024*1024
+      elif [[ $(echo $ssize | grep -i "%") ]]; then
+        tsize=$(df -Pm $devicename | grep ^/ | awk '{print $2}')
+        dsize=$(echo $ssize | tr -cd '0-9')
+        let dsize=tsize*dsize/100
+      else
+        dsize=$(echo $ssize | tr -cd '0-9')
       fi
+    else
+      dname=$modname
+      dsize=0
+    fi
+    let dsize=dsize*1024
+    if [[ $dsize -ne 0 && $dsize -lt ${sectionsize[$secnum]} ]]; then
+      while read -a readline; do
+        if [[ $secnum -eq ${readline[3]} ]]; then
+          freeupspace ${readline[2]} ${readline[1]}
+          if [[ "$TESTRUN" == "YES" || ! -e ${readline[2]} ]]; then
+            let freespace=freespace+${readline[1]}/1024
+            let sectionsize[$secnum]=${sectionsize[$secnum]}-${readline[1]}
+            if [[ $freespace -gt $setfree ]]; then
+              break
+            fi
+            if [[ $dsize -gt ${sectionsize[$secnum]} ]]; then
+              break
+            fi
+          fi
+        fi
+      done < $TEMPFILE2
     fi
   done
-  IFS="$TMPIFS"
+  # free more space if avail space is not yet reached
+  if [[ $freespace -le $setfree ]]; then
+    while read -a readline; do
+      freeupspace ${readline[2]} ${readline[1]}
+      if [[ "$TESTRUN" == "YES" || ! -e ${readline[2]} ]]; then
+        let freespace=freespace+${readline[1]}/1024
+        if [[ $freespace -gt $setfree ]]; then
+          break
+        fi
+      fi
+    done < $TEMPFILE2
+  fi
   # end loop - continue to next device
   let devnum=devnum+1
   eval devicename=\$DEVICENAME_$devnum
